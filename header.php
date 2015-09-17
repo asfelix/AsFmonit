afb518e055fbaa63f8231e4a5f6aca18b1d70a4b
asfelix <felix@ffelix.eti.br>
2015-09-17 10:19:44 -0300

    Adicionado login com Facebook

--- header.php
+++ header.php
@@ -19,7 +19,23 @@
     </head>
 
 <body>
-	
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '735732236559050',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/pt_BR/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>	
 	<div id="container">
 		<div class="starter-template">
 			<header>
