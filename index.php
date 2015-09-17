96734d0dda870fe168efd3cfc8179a5eea5bcf41
Alexsandro Felix <felix@ffelix.eti.br>
2015-09-17 10:23:54 -0300

    Removida appId por questões de segurança
    Também foi removida a URL original de redirecionamento pelas mesmas questões de segurança

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once("header.php");
 	<div class="cntd">
 		<section>
 <div class="starter-template">
-<a href="https://www.facebook.com/dialog/oauth?client_id=735732236559050&redirect_uri=http://189.85.29.238:18323/index2.php&scope=public_profile">Entrar com Facebook</a>
+<a href="https://www.facebook.com/dialog/oauth?client_id=INSERT_YOUR_APP_ID&redirect_uri=INSERT_YOUR_URL_FOR_REDIRECTION&scope=public_profile">Entrar com Facebook</a>
 <?php
 mysql_close($link);
 ?>
